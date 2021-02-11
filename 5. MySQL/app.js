98cf6b704ae69feeb852c7f3b9643156fdd4a887
Alex <a.caignard@hotmail.fr>
2021-02-11 10:47:32 +0100

    6. Création de la class Members Modif d'un membre

--- 5. MySQL/app.js
+++ 5. MySQL/app.js
@@ -1,168 +1,76 @@
 const express = require('express')
 const app = express()
 // Middleware simple qui donne des infos à chaque page chargée
-const morgan = require('morgan')
+const morgan = require('morgan')('dev')
 // On récupère les fonctions d'un autre module
-const { success, error } = require('functions')
+const { success, error, checkAndChange } = require('./assets/functions')
 // Pour POST
 const bodyParser = require('body-parser')
 // On importe la configuration du fichier json
-const config = require('./config')
-const mysql = require('mysql');
+const config = require('./assets/config')
+const mysql = require('promise-mysql');
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    database: 'node_js',
-    user: 'root',
-    password: ''
-})
+mysql.createConnection({
+    host: config.db.host,
+    database: config.db.database,
+    user: config.db.user,
+    password: config.db.password
+}).then((db) => {
+    console.log('Connected')
 
-db.connect((err) => {
-    if (err)
-        console.log(err.message)
-    else {
-        console.log('Connected')
+    // On déclare notre router
+    let MembersRouter = express.Router()
 
-        // On déclare notre router
-        let MembersRouter = express.Router()
+    // On appelle la classe Members en passant db et config en paramètres
+    let Members = require('./assets/classes/members-class')(db, config)
 
-        // Utilisation du Middleware = module qui sera lu avant chaque requête
-        app.use(morgan('dev'))
+    // Utilisation du Middleware = module qui sera lu avant chaque requête
+    app.use(morgan)
 
-        // On utilise le body-parser
-        app.use(express.json()) // for parsing application/json
-        app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+    // On utilise le body-parser
+    app.use(express.json()) // for parsing application/json
+    app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-        // On utilise le routeur pour nos éléments
-        MembersRouter.route('/:id')
-            // Fonction GET pour récupérer un membre avec son ID
-            .get((req, res) => {
-                db.query('SELECT * FROM members WHERE id = ?', [req.params.id], (err, result) => {
-                    if (err) {
-                        res.json(error(err.message))
-                    } else {
-                        if (result[0] != undefined)
-                            res.json(success(result))
-                        else
-                            res.json(error('Wrong id !'))
-                    }
-                })
-            })
-
-            // Méthode PUT pour modifier un membre avec son ID
-            .put((req, res) => {
-
-                if (req.body.name) {
-                    db.query('SELECT * FROM members WHERE id = ?', [req.params.id], (err, result) => {
-                        if (err) {
-                            res.json(error(err.message))
-                        } else {
-                            if (result[0] != undefined)
-                                db.query('SELECT * FROM members WHERE name = ? AND id != ?', [req.body.name, req.params.id], (err, result) => {
-                                    if (err) {
-                                        res.json(error(err.message))
-                                    } else {
-                                        if (result[0] != undefined)
-                                            res.json(error('same name'))
-                                        else {
-                                            db.query('UPDATE members SET name = ? WHERE id = ?', [req.body.name, req.params.id], (err, result) => {
-                                                if (err) {
-                                                    res.json(error(err.message))
-                                                } else {
-                                                    res.json(success(true))
-                                                }
-                                            })
-                                        }
-                                    }
-                                })
-                            else
-                                res.json(error('Wrong id !'))
-                        }
-                    })
-                } else {
-                    res.json(error('no name value'))
-                }
-            })
-
-            // Méthode DELETE : Suppression d'un membre avec son ID
-            .delete((req, res) => {
-                db.query('SELECT * FROM members WHERE id = ?', [req.params.id], (err, result) => {
-                    if (err) {
-                        res.json(error(err.message))
-                    } else {
-                        if (result[0] != undefined) {
-                            db.query('DELETE FROM members WHERE id = ?', [req.params.id], (err, result) => {
-                                if (err) {
-                                    res.json(error(err.message))
-                                } else {
-                                    res.json(success(true))
-                                }
-                            })
-                        } else
-                            res.json(error('Wrong id !'))
-                    }
-                })
-            })
-
-        MembersRouter.route('/')
-            // Fonction GET pour récupérer le nom des membres avec un paramètre sur le nombre
-            .get((req, res) => {
-                if (req.query.max != undefined && req.query.max > 0) {
-                    db.query('SELECT * FROM members ORDER BY id LIMIT 0, ?', [parseInt(req.query.max)], (err, result) => {
-                        if (err) {
-                            res.json(error(err.message))
-                        } else {
-                            res.json(success(result))
-                        }
-                    })
-                } else if (req.query.max != undefined) {
-                    res.json(error('Wrong max value !'))
-                } else {
-                    db.query('SELECT * FROM members ORDER BY id', (err, result) => {
-                        if (err) {
-                            res.json(error(err.message))
-                        } else {
-                            res.json(success(result))
-                        }
-                    })
-                }
-            })
-
-            // Méthode POST pour ajouter un membre
-            .post((req, res) => {
-                if (req.body.name) {
-                    db.query('SELECT * FROM members WHERE name = ?', [req.body.name], (err, result) => {
-                        if (err) {
-                            res.json(error(err.message))
-                        } else {
-                            if (result[0] != undefined) {
-                                res.json(error('name already taken'))
-                            } else {
-                                db.query('INSERT INTO members(name) VALUES(?)', [req.body.name], (err, result) => {
-                                    if (err) {
-                                        res.json(error(err.message))
-                                    } else {
-                                        res.json(success({
-                                            id: result.insertId,
-                                            name: req.body.name
-                                        }))
-                                    }
-                                })
-                            }
-                        }
-                    })
-                } else {
-                    res.json(error('no name value'))
-                }
-            })
-
-        // On indique le lien et le routeur utilisé
-        app.use(config.rootAPI + 'members', MembersRouter)
-
-        // On écoute sur le port indiqué
-        app.listen(config.port, () => {
-            console.log('Start on port ' + config.port)
+    // On utilise le routeur pour nos éléments
+    MembersRouter.route('/:id')
+        // Fonction GET pour récupérer un membre avec son ID
+        .get(async (req, res) => {
+            let member = await Members.getByID(req.params.id)
+            res.json(checkAndChange(member))
         })
-    }
 
+        // Méthode PUT pour modifier un membre avec son ID
+        .put(async (req, res) => {
+            let updateMember = await Members.update(req.params.id, req.body.name)
+            res.json(checkAndChange(updateMember))
+        })
+
+        // Méthode DELETE : Suppression d'un membre avec son ID
+        .delete((req, res) => {
+            
+        })
+
+    MembersRouter.route('/')
+        // Fonction GET pour récupérer le nom des membres avec un paramètre sur le nombre
+        .get(async (req, res) => {
+            let allMembers = await Members.getAll(req.query.max)
+            res.json(checkAndChange(allMembers))
+        })
+
+        // Méthode POST pour ajouter un membre
+        .post(async (req, res) => {
+            let addMember = await Members.add(req.body.name)
+            res.json(checkAndChange(addMember))
+        })
+
+    // On indique le lien et le routeur utilisé
+    app.use(config.rootAPI + 'members', MembersRouter)
+
+    // On écoute sur le port indiqué
+    app.listen(config.port, () => {
+        console.log('Start on port ' + config.port)
+    })
+}).catch((err) => {
+    console.log('Error during database connection')
+    console.log(err.message)
 })
